e849cfc42f754140506a2f4c0ec8211a58c62689
Anand Raj <anandraj7008@gmail.com>
2026-04-04 03:13:00 +0530

    my add to the root

--- app/services1/[serviceType]/[serviceCategory]/page.js
+++ app/services1/[serviceType]/[serviceCategory]/page.js
@@ -1,18 +1,18 @@
 
-import HeroSection from '../../components/service/heroSection';
-import ServiceType from '../../components/service/ServiceType';
-import ServiceCategory from '../../components/service/serviceCategory';
-import ClientsSection from '../../components/service/ClientsSection';
-import Navbar from '../../components/service/Navbar';
+// import HeroSection from '../../components/service/heroSection';
+// import ServiceType from '../../components/service/ServiceType';
+// import ServiceCategory from '../../components/service/serviceCategory';
+// import ClientsSection from '../../components/service/ClientsSection';
+// import Navbar from '../../components/service/Navbar';
 
-export default function Service() {
-  return (
-   <>
-   <Navbar/>
-   <HeroSection/>
-   <ServiceType/>
-   <ServiceCategory/>
-   <ClientsSection/>
-  </>
-  )
-}
+// export default function Service() {
+//   return (
+//    <>
+//    <Navbar/>
+//    <HeroSection/>
+//    <ServiceType/>
+//    <ServiceCategory/>
+//    <ClientsSection/>
+//   </>
+//   )
+// }
